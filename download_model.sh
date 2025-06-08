95dc726896b0336ebf31e154d60276709656ce80
Andrea Janes <janes.andrea@gmail.com>
2025-06-08 23:26:55 +0200

    Update

--- download_model.sh
+++ download_model.sh
@@ -2,6 +2,7 @@ mkdir -p all-MiniLM-L6-v2
 cd all-MiniLM-L6-v2
 
 wget https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2/resolve/main/config.json
+wget https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2/resolve/main/modules.json
 wget https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2/resolve/main/pytorch_model.bin
 wget https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2/resolve/main/sentence_bert_config.json
 wget https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2/resolve/main/special_tokens_map.json
